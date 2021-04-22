def63238202ad7f88e0eb3e88f15458242bcfbc8
x4fx77x4f <70858634+x4fx77x4f@users.noreply.github.com>
2021-04-21 22:38:06 -0400

    overhaul debug text and add fallback for nonexistent assets

--- init.lua
+++ init.lua
@@ -6,6 +6,28 @@ sprintf = string.format
 function printf(...)
 	return print(sprintf(...))
 end
+function errorf(...)
+	return error(sprintf(...))
+end
+function assertf(cond, ...)
+	if cond then
+		return cond
+	end
+	return error(sprintf(...))
+end
+function boobytrap(...)
+	local msg = sprintf(...)
+	return function()
+		return error(msg)
+	end
+end
+local dbgStr
+function dbgprintf(...)
+	dbgStr = dbgStr..sprintf(...).."\n"
+end
+function dbgprintf2(...)
+	dbgStr = sprintf(...).."\n"..dbgStr
+end
 
 local SF_USE_INCLUDEDATA = true
 local SF_CACHE_INCLUDEDATA = true
@@ -33,6 +55,7 @@ local function SF_PATH_HASH(path)
 	return sprintf(SF_INCLUDE_PREFIX..'_%s.%s', crc(path), ext)
 end
 local SF_PATH_USABLE = {}
+local SF_PATH_PLACEHOLDER = SF_PATH_HASH('/placeholder.png')
 function SF_PATH_PATCH(path)
 	if SF_USE_INCLUDEDATA then
 		local newpath = SF_PATH_USABLE[path]
@@ -53,11 +76,22 @@ function SF_PATH_PATCH(path)
 			return newpath
 		end
 	end
-	return 'data/sf_filedata/'..SF_INCLUDE_PREFIX..'/'..path
+	local newpath = SF_INCLUDE_PREFIX..'/'..path
+	if not hasPermission('file.exists') or not file.exists(newpath) then
+		return SF_PATH_PLACEHOLDER
+	end
+	return 'data/sf_filedata/'..newpath
 end
+--@includedata sm64sf/placeholder.png
+local hash = SF_PATH_HASH('/placeholder.png')
+SF_PATH_PLACEHOLDER = file.existsTemp(hash) or file.writeTemp(hash, getScripts()['sm64sf/placeholder.png'])
 
 --@include sm64sf/include/pr/gbi.lua
 require('sm64sf/include/pr/gbi.lua')
+--@include sm64sf/include/level_commands.lua
+require('sm64sf/include/level_commands.lua')
+--@include sm64sf/levels/entry.lua
+require('sm64sf/levels/entry.lua')
 --@include sm64sf/levels/intro/leveldata.lua
 require('sm64sf/levels/intro/leveldata.lua')
 --@include sm64sf/levels/intro/script.lua
@@ -68,19 +102,12 @@ require('sm64sf/src/menu/intro_geo.lua')
 require('sm64sf/src/engine/level_script.lua')
 --@include sm64sf/src/game/game_init.lua
 require('sm64sf/src/game/game_init.lua')
+--@include sm64sf/src/game/object_list_processor.lua
+require('sm64sf/src/game/object_list_processor.lua')
+--@include sm64sf/src/game/area.lua
+require('sm64sf/src/game/area.lua')
 
-local thread = coroutine.create(function()
-    geo_intro_super_mario_64_logo(0)
-    geo_intro_tm_copyright(0)
-    while true do
-        rendering_init()
-        geo_intro_super_mario_64_logo(1)
-        geo_intro_tm_copyright(1)
-        display_and_vsync()
-        display_frame_buffer()
-        coroutine.yield(true)
-    end
-end)
+local thread = coroutine.create(thread5_game_loop)
 
 local threshold = quotaMax()*0.5
 local waitUntil = 0
@@ -94,57 +121,55 @@ local bg = Color(31, 31, 31)
 render.createRenderTarget('screen')
 render.createRenderTarget('final')
 hook.add('render', '', function()
-    render.setBackgroundColor(bg)
-    --render.setFilterMag(TEXFILTER.POINT)
-    --render.setFilterMin(TEXFILTER.POINT)
-    
-    local now = timer.systime()
-    if now >= frameExpire then
-        fps = frames
-        frames = 0
-        frameExpire = now+1
-    end
-    if now >= waitUntil then
-        render.selectRenderTarget('screen')
-            while quotaUsed() < threshold do
-                if coroutine.resume(thread) then
-                    frames = frames+1
-                    break
-                end
-            end
-            waitUntil = now+frameTime
-        render.selectRenderTarget()
-    end
-    
-    render.setRGBA(255, 255, 255, 255)
-    render.setRenderTargetTexture('final')
-    render.drawTexturedRectUV(0, 63, 512, 384, 0, 0, 0.3125, 0.234375)
-    
-    -- material debug
-    --[[
-    render.setMaterial(intro_seg7_texture_07007EA0)
-    render.drawTexturedRect(0, 0, 32, 32)
-    render.setMaterial(intro_seg7_texture_070086A0)
-    render.drawTexturedRect(32, 0, 32, 32)
-    render.setMaterial(intro_seg7_texture_0700B4A0)
-    render.drawTexturedRect(64, 0, 128, 16)
-    render.setMaterial(intro_seg7_texture_0700C4A0)
-    render.drawTexturedRect(192, 0, 16, 16)
-    --]]
-    
-    -- debug text
-    -- [[
-    local dbgstr = string.format(
-        "fps: %d\n"..
-        "gGlobalTimer: %d\n"..
-        "sIntroFrameCounter: %d\n"..
-        "sTmCopyrightAlpha: %f",
-        fps,
-        gGlobalTimer or -1,
-        sIntroFrameCounter or -1,
-        sTmCopyrightAlpha or -1
-    )
-    render.setFont('DebugFixed')
-    render.drawText(0, 0, dbgstr)
-    --]]
+	render.setBackgroundColor(bg)
+	--render.setFilterMag(TEXFILTER.POINT)
+	--render.setFilterMin(TEXFILTER.POINT)
+	
+	dbgStr, dbgStrLines = "", 0
+	
+	local now = timer.systime()
+	if now >= frameExpire then
+		fps = frames
+		frames = 0
+		frameExpire = now+1
+	end
+	if now >= waitUntil then
+		render.selectRenderTarget('screen')
+			while quotaUsed() < threshold do
+				if coroutine.resume(thread) then
+					frames = frames+1
+					break
+				end
+			end
+			waitUntil = now+frameTime
+		render.selectRenderTarget()
+	end
+	
+	render.setRGBA(255, 255, 255, 255)
+	render.setRenderTargetTexture('final')
+	render.drawTexturedRectUV(0, 63, 512, 384, 0, 0, 0.3125, 0.234375)
+	
+	-- material debug
+	--[[
+	render.setMaterial(intro_seg7_texture_07007EA0)
+	render.drawTexturedRect(0, 0, 32, 32)
+	render.setMaterial(intro_seg7_texture_070086A0)
+	render.drawTexturedRect(32, 0, 32, 32)
+	render.setMaterial(intro_seg7_texture_0700B4A0)
+	render.drawTexturedRect(64, 0, 128, 16)
+	render.setMaterial(intro_seg7_texture_0700C4A0)
+	render.drawTexturedRect(192, 0, 16, 16)
+	--]]
+	
+	-- debug text
+	dbgprintf2("fps: %d", fps)
+	dbgprintf2("quota: %d%%", math.ceil(quotaAverage()/quotaMax()*100))
+	-- [[
+	dbgStr = string.gsub(dbgStr, "\n$", "")
+	render.setFont('DebugFixed')
+	render.setRGBA(0, 0, 0, 255)
+	render.drawRectFast(0, 0, render.getTextSize(dbgStr))
+	render.setRGBA(255, 255, 255, 255)
+	render.drawText(0, 0, dbgStr)
+	--]]
 end)
