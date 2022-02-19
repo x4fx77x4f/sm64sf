fbf28b9be0ec523b5915fbbcf98b99974c8fa277
x4fx77x4f <70858634+x4fx77x4f@users.noreply.github.com>
2022-02-19 02:11:17 -0500

    implement intro_seg7_dl_0700C6A0 properly (you can now SEE the legal info!!)

--- levels/intro/leveldata.lua
+++ levels/intro/leveldata.lua
@@ -1,6 +1,23 @@
 -- The equivalents of this file in n64decomp/sm64 are 'src/levels/intro/leveldata.c'
 -- The equivalent of this file in sm64js is 'src/levels/intro/leveldata.js'
 
+local scripts = getScripts()
+local root
+for path in pairs(scripts) do
+	local i = string.find(path, '/init.lua', nil, true)
+	if i then
+		root = string.sub(path, 1, i) -- e.g. "sm64sf2/"
+		break
+	end
+end
+assert(root, "could not find root")
+local function just_figure_it_out_please(path)
+	local temppath = 'sm64sf2_'..string.gsub(path, '/', '_')
+	temppath = file.existsTemp(temppath) or file.writeTemp(temppath, scripts[root..path])
+	printf("[just_figure_it_out_please] temppath: %q\n", temppath)
+	return material.createFromImage('../'..temppath, '')
+end
+
 -- 0x0700B3A0 - 0x0700B420
 function intro_seg7_dl_0700B3A0()
 	render.setRGBA(127, 255, 127, 255)
@@ -10,24 +27,37 @@ end
 
 -- 0x0700B4A0 - 0x0700B4A2
 if VERSION_EU or VERSION_SH then
-	intro_seg7_texture_0700B4A0 = 'levels/intro/2_eu_copyright.rgba16.inc.c'
+	--@includedata ./2_eu_copyright.rgba16.png
+	intro_seg7_texture_0700B4A0 = 'levels/intro/2_eu_copyright.rgba16.png'
 else
-	intro_seg7_texture_0700B4A0 = 'levels/intro/2_copyright.rgba16.inc.c'
+	--@includedata ./2_copyright.rgba16.png
+	intro_seg7_texture_0700B4A0 = 'levels/intro/2_copyright.rgba16.png'
 end
+intro_seg7_texture_0700B4A0 = just_figure_it_out_please(intro_seg7_texture_0700B4A0)
 
 -- 0x0700C4A0 - 0x0700D4A0
 if VERSION_EU then
-	intro_seg7_texture_0700C4A0 = "levels/intro/3_eu_tm.rgba16.inc.c"
+	--@includedata ./3_eu_tm.rgba16.png
+	intro_seg7_texture_0700C4A0 = 'levels/intro/3_eu_tm.rgba16.png'
 elseif VERSION_SH then
-	intro_seg7_texture_0700C4A0 = "levels/intro/3_sh_tm.rgba16.inc.c"
+	--@includedata ./3_sh_tm.rgba16.png
+	intro_seg7_texture_0700C4A0 = 'levels/intro/3_sh_tm.rgba16.png'
 else
-	intro_seg7_texture_0700C4A0 = 'levels/intro/3_tm.rgba16.inc.c'
+	--@includedata ./3_tm.rgba16.png
+	intro_seg7_texture_0700C4A0 = 'levels/intro/3_tm.rgba16.png'
 end
+intro_seg7_texture_0700C4A0 = just_figure_it_out_please(intro_seg7_texture_0700C4A0)
 
 -- 0x0700C6A0 - 0x0700C790
 function intro_seg7_dl_0700C6A0()
-	render.setFont('DermaDefault')
-	render.drawText(SCREEN_WIDTH/2, SCREEN_HEIGHT/4*3, "intro_seg7_dl_0700C6A0", TEXT_ALIGN.CENTER, TEXT_ALIGN.CENTER)
+	-- Look up intro_seg7_vertex_0700B420 in n64decomp/sm64 to see what the original data looked like.
+	-- Seems like the positive Y is up, not down, in N64-land, so... new_y=screen_height-original_y-original_height
+	
+	render.setMaterial(intro_seg7_texture_0700B4A0)
+	render.drawTexturedRect(96, SCREEN_HEIGHT-58, 128, 16)
+	
+	render.setMaterial(intro_seg7_texture_0700C4A0)
+	render.drawTexturedRect(268, SCREEN_HEIGHT-196, 16, 16)
 end
 
 -- 0x0700C790
